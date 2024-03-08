38f03137274f1d5bcc281a910a74656be2bdf1dd
AlexandreMoraez <130381131+AlexandreMoraez@users.noreply.github.com>
2024-03-07 21:44:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const mpd = document.querySelector("#mpd");
 const cons = document.querySelector("#cons");
 const ongs = document.querySelector(".ongs");
+const pbls = document.querySelector(".maispbls");
 const btn_ong = document.querySelector("#btn_ong");
+const btn_pbl = document.querySelector("#btn_pbl");
 
 mpd.addEventListener("mouseover", () => {
     mpd.style.border = "3px solid #20973a"; 
@@ -20,5 +22,17 @@ btn_ong.addEventListener("click", () => {
     else {
         ongs.style.animationName = "ong";
         ongs.style.display = "block";
+    }
+})
+btn_pbl.addEventListener("click", () => {
+    if(pbls.style.animationName == "pbls") {
+    pbls.style.animationName = "pblsback";
+    setTimeout(() => {
+        pbls.style.display = "none";
+    }, 400)
+    }
+    else {
+        pbls.style.animationName = "pbls";
+        pbls.style.display = "block";
     }
 })
